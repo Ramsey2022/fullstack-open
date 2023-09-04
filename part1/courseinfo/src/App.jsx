fe88aada6ea2a9827f9d51bf48aac28f1d660279
Ramsey2022 <abovethelotus@yahoo.com>
2023-09-04 16:51:36 -0700

    finished exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -7,13 +7,31 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p> {props.name}, {props.exercises} exercises.</p>
+    </div>
+  )
+}
+
 const Content = (props) => {
   console.log(props)
   return (
     <div>
-      <p> {props.part1}, {props.exercises1} exercises.</p>
-      <p> {props.part2}, {props.exercises2} exercises.</p>
-      <p> {props.part3}, {props.exercises3} exercises.</p>
+      <Part
+      name={props.part1}
+      exercises={props.exercises1} 
+      />
+      <Part
+      name={props.part2}
+      exercises={props.exercises2} 
+      />
+      <Part
+      name={props.part3}
+      exercises={props.exercises3} 
+      />
     </div>
   )
 }
