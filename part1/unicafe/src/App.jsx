baad816a29475178debc73af64a82367b393fb45
Ramsey2022 <abovethelotus@yahoo.com>
2023-09-07 14:45:34 -0700

    unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,14 +2,30 @@ import { useState } from 'react'
 
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <br />
+      <p>Good: {props.good}</p>
+      <p>Neutral: {props.neutral}</p>
+      <p>Bad: {props.bad}</p>
+      <p>All: {props.all}</p>
+      <p>Average: {props.average}</p>
+      <p>Positive: {props.positive}</p>
+      <p>Negative: {props.negative}</p>
+    </div>
+  )
+}
+
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
   
+  const all = (good + neutral + bad)
   const average = (good - bad) / all
   const positive = (good * 100) / all
   const negative = (bad * 100) / all
@@ -17,19 +33,16 @@ const App = () => {
   const handleGoodClick = () => {
     const updatedGood = good + 1
     setGood(updatedGood)
-    setAll(updatedGood + neutral + bad)
   }
   
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
-    setAll(good + updatedNeutral + bad)
   }
   
   const handleBadClick = () => {
     const updatedBad = bad + 1
     setBad(updatedBad)
-    setAll(good + neutral + updatedBad)
   }
 
 
@@ -48,15 +61,15 @@ const App = () => {
       handleClick={handleBadClick}
       text='Bad'
       />
-      <h1>Statistics</h1>
-      <br />
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {all}</p>
-      <p>Average: {average}</p>
-      <p>Positive: {positive}</p>
-      <p>Negative: {negative}</p>
+      <Statistics
+      good={good}
+      neutral={neutral}
+      bad={bad}
+      all={all}
+      average={average}
+      positive={positive}
+      negative={negative}
+      />
     </div>
   )
 }
