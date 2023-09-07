f707714457203b9fa243183a54ea523f5c9af8f0
Ramsey2022 <abovethelotus@yahoo.com>
2023-09-07 14:29:51 -0700

    step 2 of unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,21 +8,30 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  
+  const average = (good - bad) / all
+  const positive = (good * 100) / all
+  const negative = (bad * 100) / all
 
   const handleGoodClick = () => {
     const updatedGood = good + 1
     setGood(updatedGood)
+    setAll(updatedGood + neutral + bad)
   }
   
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
+    setAll(good + updatedNeutral + bad)
   }
   
   const handleBadClick = () => {
-    const updatedBadClick = bad + 1
-    setBad(updatedBadClick)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setAll(good + neutral + updatedBad)
   }
+
 
   return (
     <div>
@@ -44,6 +53,10 @@ const App = () => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive}</p>
+      <p>Negative: {negative}</p>
     </div>
   )
 }
