a0d5da1f8d5de5832fe2f328a7c3b0a26c49128f
Ramsey2022 <abovethelotus@yahoo.com>
2023-09-10 16:03:33 -0700

    finished phonebook step 1 and 2

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import Person from './components/Person'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' },
+    { name: 'Ada Lovelace' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) =>{
+    event.preventDefault()
+    console.log('button clicked', event.target)
+
+    if (
+      persons.some(
+        (person) => person.name.toLowerCase() === newName.toLowerCase()
+      )
+    ) {
+      alert(`${newName} is already added to phonebook`)
+      setNewName('')
+      return
+    }
+
+    const personObject = { name: newName }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handlePersonChange = (e) => {
+    console.log(e.target.value)
+    setNewName(e.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name:  
+          <input
+          value={newName}
+          onChange={handlePersonChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person =>
+        <Person key={person.name} person={person} />
+      )}
+    </div>
+  )
+}
+
+export default App
