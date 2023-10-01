6ef070e5ce3e9500f1cc112065b3a113a71c5a39
Ramsey2022 <abovethelotus@yahoo.com>
2023-10-01 15:57:22 -0700

    phonebook step 8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 
-app.use(morgan('tiny'))
+morgan.token('body', request => {
+  return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.json())
+
+
 
 
 const PORT = 3001
