c43e27f2056a8e3f3854c8a53cba3294c2bc08b5
Ramsey2022 <abovethelotus@yahoo.com>
2023-10-01 15:47:04 -0700

    added morgan middleware to phonebook

--- part3/example/index.js
+++ part3/example/index.js
@@ -3,6 +3,18 @@ const app = express()
 
 app.use(express.json())
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
 let notes = [
   {
     id: 1,
@@ -79,3 +91,6 @@ app.post('/api/notes', (request, response) => {
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
+
+app.use(requestLogger)
+app.use(unknownEndpoint)
